b57379fcaab361ac4d9ef59ccfc69cfb2b1b4676
SadraMesbah <sadra.mesbah@gmail.com>
2024-11-27 19:38:45 -0500

    add application context verification test

--- src/main/java/com/sadramesbah/asynchronous_comunicating_agents/AsynchronousCommunicatingAgentsApplication.java
+++ src/main/java/com/sadramesbah/asynchronous_comunicating_agents/AsynchronousCommunicatingAgentsApplication.java
@@ -2,12 +2,13 @@ package com.sadramesbah.asynchronous_comunicating_agents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AsynchronousCommunicatingAgentsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AsynchronousCommunicatingAgentsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AsynchronousCommunicatingAgentsApplication.class, args);
+  }
 
 }
--- src/test/java/com/sadramesbah/asynchronous_comunicating_agents/AsynchronousCommunicatingAgentsApplicationTests.java
+++ src/test/java/com/sadramesbah/asynchronous_comunicating_agents/AsynchronousCommunicatingAgentsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.sadramesbah.asynchronous_comunicating_agents;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class AsynchronousCommunicatingAgentsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private ApplicationContext applicationContext;
 
+  @Test
+  void contextLoads() {
+    assertNotNull(applicationContext, "Failed to load the application context.");
+  }
 }
