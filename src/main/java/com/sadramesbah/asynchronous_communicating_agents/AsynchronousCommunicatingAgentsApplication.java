353936389cc755054b40be99b04c9b294019f0ef
SadraMesbah <sadra.mesbah@gmail.com>
2026-07-05 00:59:13 -0400

    update spring-boot-starter-parent version and switch to MongoDB data access

--- src/main/java/com/sadramesbah/asynchronous_communicating_agents/AsynchronousCommunicatingAgentsApplication.java
+++ src/main/java/com/sadramesbah/asynchronous_communicating_agents/AsynchronousCommunicatingAgentsApplication.java
@@ -2,9 +2,8 @@ package com.sadramesbah.asynchronous_communicating_agents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class AsynchronousCommunicatingAgentsApplication {
 
   public static void main(String[] args) {
